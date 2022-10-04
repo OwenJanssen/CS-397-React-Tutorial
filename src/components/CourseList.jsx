0f5c286f137b1c1c23e3f06a100b4d0511571165
OwenJanssen <67287024+OwenJanssen@users.noreply.github.com>
2022-10-04 15:49:15 -0500

    Course selection

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,7 +1,27 @@
-const CourseList = ({courses}) => (
-    <div className="course-list">
-        { courses.map(([_, course]) => (
-            <div className="card m-1 p-2" style={{width: '12rem'}} key={course.number}>
+import { useState } from "react";
+
+const CourseList = ({courses}) => {
+    const [selectedList, setSelectedList] = useState([]);
+
+    const updateSelectedList = (courseNumber) => {
+        if (selectedList.includes(courseNumber)) {
+            setSelectedList(lst => lst.push(courseNumber));
+        }
+        else {
+            setSelectedList(lst => lst.filter(cn => cn != courseNumber));
+        }
+    }
+    
+    return <div className="course-list">
+        { courses.map(([_, course]) => {
+            const [backgroundColor, setBackgroundColor] = useState('white');
+
+            const updateSelected = () => {
+                setBackgroundColor(backgroundColor === 'white' ? 'deepskyblue' : 'white');
+                updateSelectedList(course.number);
+            };
+
+            return <div className="card m-1 p-2" style={{width: '12rem', backgroundColor: backgroundColor}} key={course.number} onClick={updateSelected}>
                 <div className="card-body" style={{display: 'flex', flexFlow: 'column'}}>
                     <h5 className="card-title">
                         {`${course.term} CS ${course.number}`}
@@ -14,9 +34,9 @@ const CourseList = ({courses}) => (
                         {course.meets}
                     </div>
                 </div>
-            </div>
-        ))}
+            </div>;
+        })}
     </div>
-);
+};
 
 export default CourseList;
