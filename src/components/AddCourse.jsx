49cbbaca8afa297bbc5eb4e99201da3acbce864e
OwenJanssen <67287024+OwenJanssen@users.noreply.github.com>
2022-10-12 22:10:24 -0500

    Form Validation

--- src/components/AddCourse.jsx
+++ src/components/AddCourse.jsx
@@ -1,6 +1,28 @@
+import {useState} from 'react';
+
 const AddCourse = () => {
+    const [title, setTitle] = useState("");
+    const [meetingTime, setMeetingTime] = useState("");
+
+    const updateTitle = (event) => {
+        setTitle(event.target.value);
+    };
+
+    const updateMeetingTime = (event) => {
+        setMeetingTime(event.target.value);
+    };
+
     const goBack = (event) => {
         window.location.href = "/";
+    };
+
+    const titleInvalid = () => {
+        return title.length < 2;
+    };
+
+    const meetingTimeInvalid = () => {
+        const re = /([M|W|F|Th|Tu]+)\s(\d+):(\d\d)-(\d+):(\d\d)/;
+        return meetingTime.length > 0 && !re.test(meetingTime);
     }
 
     return <div className="container">
@@ -9,12 +31,14 @@ const AddCourse = () => {
         <form>
             <div>
                 <label style={{marginBottom: "20px", marginRight: "5px"}}>Title:</label>
-                <input type="text"/>
+                <input type="text" value={title} onChange={updateTitle}/>
+                {titleInvalid() && <div style={{marginBottom: "20px", background: 'red', width: 'fit-content', whiteSpace: 'pre'}}> Course title must be at least 2 characters long </div>}
             </div>
 
             <div>
                 <label style={{marginBottom: "20px", marginRight: "5px"}}>Meets:</label>
-                <input type="text"/>
+                <input type="text" value={meetingTime} onChange={updateMeetingTime}/>
+                {meetingTimeInvalid() && <div style={{marginBottom: "20px", background: 'red', width: 'fit-content', whiteSpace: 'pre'}}> Meeting time must contain days and start-end, e.g., MWF 12:00-13:20 </div>}
             </div>
         </form>
 
